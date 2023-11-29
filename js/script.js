411730ed1b957e13ac1e0b38dddc7c5343e46cfd
tomasdarienko <darienkotomas@gmail.com>
2023-11-29 23:05:58 +0100

    finito (numerato e stilizzato)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
 
 let genera = document.getElementById('genera')
+
 genera.addEventListener('click',function(){
 
-     
      function quadrato(num) {
           const div = document.createElement('div');
      
           div.classList.add('quadrato');
      
+          div.innerHTML=num
           return div
      }
      const griglia = document.getElementById('grid');
      
      
      for (i = 0; i < 100; i++) {
-          let quad = quadrato();
+          let quad = quadrato(i+1);
           griglia.appendChild(quad)
      }
 })
